90992cd4e5f608eac5fc25c2b804a897a2d2d2f1
iscomi <isacomrie@alum.us.es>
2022-02-12 23:59:54 +0100

    cambio 1 en main

--- comun.py
+++ comun.py
@@ -1,3 +1,5 @@
 import modulo_inicial as inicial
+import modulo_main as main
 
 inicial.funcion_inicial()
+main.funcion_main_1()
---
+++ modulo_main.py
@@ -0,0 +1,3 @@
+def funcion_main_1():
+	print("Esta es la funcion_main_1 incluida en el cambio 1 de la rama main")
+	return None
