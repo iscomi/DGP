7a2e318df134adaa78b01f2658a60b034a64b462
iscomi <isacomrie@alum.us.es>
2022-02-13 00:05:32 +0100

    cambio 1 en branchB

--- comun.py
+++ comun.py
@@ -1,3 +1,5 @@
 import modulo_inicial as inicial
+import modulo_branchB as branchB
 
 inicial.funcion_inicial()
+branchB.funcion_branchB_1()
---
+++ modulo_branchB.py
@@ -0,0 +1,3 @@
+def funcion_branchB_1():
+	print("Esta es la funcion_branchB_1 incluida en el cambio 1 de la rama branchB")
+	return None
